c9526be7f370b63750181e46c1b711de3ff07792
siqueira76 <josecarlos.siqueira76@gmail.com>
2024-03-22 20:43:17 -0300

    First commit

---
+++ src/main/java/com/siqueira76/samplerestapirailway/SampleRestApiRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.siqueira76.samplerestapirailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestApiRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestApiRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siqueira76/samplerestapirailway/SampleRestApiRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siqueira76.samplerestapirailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRestApiRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
